61801fafb12f461696853bf26857a09805862ec6
FiloPL <tzegarlicki@gmail.com>
2022-05-09 22:26:12 +0200

    Initial commit

---
+++ src/main/java/pl/filo/brewberyrestapi/BrewberyRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.filo.brewberyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewberyRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrewberyRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/filo/brewberyrestapi/BrewberyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filo.brewberyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewberyRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
